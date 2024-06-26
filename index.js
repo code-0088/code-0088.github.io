4efd140c9dbab3425467e8ab1bb9626ec135f369
code-0088 <82009964+code-0088@users.noreply.github.com>
2024-06-26 00:02:27 -0400

    api metro json

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const typewriterText = document.getElementById('typewriter-text');
-  const text = "I am a developer...";
-  let index = 0;
-
-  function type() {
-    if (index < text.length) {
-      typewriterText.textContent += text.charAt(index);
-      index++;
-      setTimeout(type, 100); // Velocidad de tipeo
-    } else {
-      setTimeout(() => {
-        index = 0;
-        typewriterText.textContent = '';
-        setTimeout(type, 1000); // Pausa antes de reiniciar
-      }, 2000); // Pausa después de completar el texto
-    }
-  }
-
-  type();
-});
 let data = [
   { id: 1, nombre: "Lugar A", latitud: 40.7128, longitud: -74.0060 },
   { id: 2, nombre: "Lugar B", latitud: 34.0522, longitud: -118.2437 },
